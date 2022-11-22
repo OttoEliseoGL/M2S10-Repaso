92eab3029a4401babd069d2f09d80a6b63610d55
OttoEliseoGL <otto_eliseo@outlook.es>
2022-11-21 20:04:03 -0600

    Done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
 console.log("Entro al main.js");
+
+const body = document.querySelector("body");
+
+function btnCambiarColor() {
+    console.log("Entró al botón")
+    // setTimeout(cambiarColor, 30);
+    setInterval(cambiarColor, 2000);
+    // cambiarColor()
+}
+
+function cambiarColor() {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    // console.log("Mi numero aleatorio es: " + randomColor);
+    body.style.backgroundColor = `#${randomColor}`;
+}
